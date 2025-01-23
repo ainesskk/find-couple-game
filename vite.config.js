e33b943c0385ea5103cf0c9dc7eacd44b0880c74
ainesk <ksenia_krr@mail.ru>
2025-01-23 20:39:02 +0300

    fix base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/find-couple/',
+  base: '/find-couple-game/',
   plugins: [react()],
 })
